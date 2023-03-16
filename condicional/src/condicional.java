c0ee533ec90df9ae365ba091d207eb10f623a117
LuIsMoReNo7 <1483853@senati.pe>
2023-03-16 09:58:14 -0500

    ingresamos el valor

--- condicional/src/condicional.java
+++ condicional/src/condicional.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class condicional {
   public static void main(String[] args) {
-
+    Scanner entrada = new Scanner(System.in); // Declaramos la clase "Scanner"
+    // Pedimos el numero
+    System.out.println("Ingrese el numero : ");
+    // "nextInt()" lee valores int introducidos por el usario
+    int numero1 = entrada.nextInt();
      }
   }
