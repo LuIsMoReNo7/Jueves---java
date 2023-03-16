6ce0b328d5476627bdeecf9103ed90cdad3f47ef
LuIsMoReNo7 <1483853@senati.pe>
2023-03-16 10:48:50 -0500

    hacemos para que nosotros ingresemos el nombre

---
+++ condicional/src/condicional_2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class condicional_2 {
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in); // Declaramos la clase "Scanner"
+    System.out.println("Ingrese el nombre");
+    String nombre = entrada.next();
+    // Pedimos el numero
+    System.out.println("Ingrese el numero : ");
+    // "nextInt()" lee valores int introducidos por el usario
+    int numero1 = entrada.nextInt();
+    // usamos la condicional if
+    if (numero1 < 10){
+      //imprimimos
+     System.out.println("Hola" + nombre);
+     System.out.println("El valor que ingresaste es menor que 10");
+     }
+     else {
+      System.out.println("Hola" + nombre);
+     System.out.println("El valor que ingresaste es mayor que 10");
+     }
+       
+       }
+     
+     }
+    
+  
